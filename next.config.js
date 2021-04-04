1854fd229a7f39124622e0411095f113482cebd3
joanjeremiah <81401721+joanjeremiah@users.noreply.github.com>
2021-04-04 18:50:53 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
         GOOGLE_CLIENT_ID : "368583127834-lkqlpo7snugmcrj9eijqv7ee0iattj7v.apps.googleusercontent.com",
         GOOGLE_CLIENT_SECRET : "4fVjagDUodj3OptU2W-28HOc",
         mongodburl: "mongodb+srv://rsanj:rsanj@cluster0.1luod.mongodb.net/Waykey",
-        NEXTAUTH_URL: "https://example.com"
+        NEXTAUTH_URL: "https://waykey.vercel.app"
     }
 };
